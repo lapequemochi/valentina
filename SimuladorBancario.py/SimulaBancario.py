803900190425f4efe978f0421069858828f48d2c
valentina <lapequemochi@gmail.com>
2024-02-26 21:50:17 -0500

    COMANDOS NUEVOS

--- SimuladorBancario.py/SimulaBancario.py
+++ SimuladorBancario.py/SimulaBancario.py
@@ -6,3 +6,27 @@ class SimulaBancario :
     cedula = ""
     nombre = ""
     mesActual = 0    
+    def CambiarConsignacionValor (self,NuevaConsignacionValor):
+        #Aqui va el codigo de la nueva consignacion
+        return 0
+    def CambiarRetirarValor (self,NuevoRetirarValor):
+        #Aqui va el codigo del retiro
+        return 0
+    def CambiarInteresMensual (self,NuevoInteresMensual):
+        #Aqui va el codigo del Interes
+        return 0
+    def CambiarSimulaBancario (self,nNuevoConsignacionValor, nNuevoRetirarValor, nNuevoInteresMensual):
+        #Aqui va el codigo del Simulador Bancario
+        return 0
+    def ConsultarConsignacionValor (self):
+        #Aqui va el codigo de la consulta de la Consignacion
+        return self.ConsultarConsignacionValor
+    def ConsultarRetirarValor (self):
+        #Aqui va el codigo de la consulta del Retiro
+        return self.ConsultarRetirarValor
+    def ConsultarInteresMensual (self):
+        #Aqui va el codigo de la consulta del interes
+        return self.ConsultarInteresMensual
+    
+
+     
